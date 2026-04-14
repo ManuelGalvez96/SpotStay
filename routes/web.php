f0296fdbf42f31af0d4f50def6fbe04f592dcc40
ManuelGalvez96 <461595.joan23@fje.edu>
2026-04-14 16:16:36 +0200

    dashboard arrendador

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 });
+
+Route::get('/arrendador/dashboard', function () {
+    return view('landlord.dashboard');
+});
+

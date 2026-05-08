06332a797095ef2fe0c0063ec66c0eb8bd0f6e13
HugoFI <100006119.joan23@fje.edu>
2026-05-08 16:59:54 +0200

    Arreglar problemas

--- resources/views/layouts/miembro.blade.php
+++ resources/views/layouts/miembro.blade.php
@@ -23,8 +23,8 @@
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('js/miembro/miembro.js') }}"></script>
     @yield('scripts')
+    <script src="{{ asset('js/miembro/miembro.js') }}"></script>
 </body>
 
 </html>

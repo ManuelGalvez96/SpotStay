208943acdbaa5cfdbcf2ab0a28dc593a4ab36aec
SergiNebot <440244.joan23@fje.edu>
2026-05-13 20:20:34 +0200

    modificaciones

--- config/database.php
+++ config/database.php
@@ -57,8 +57,8 @@ return [
             'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => 'innoDB',
+            'strict' => false,
+            'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],

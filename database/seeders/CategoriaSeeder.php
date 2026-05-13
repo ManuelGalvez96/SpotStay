4871318f539abcf752bd52f939d2e115a161fc44
JLavignole <445647.joan23@fje.edu>
2026-05-13 18:25:02 +0200

    fix: Agregar Climatización a CategoriaSeeder
    Faltaba la categoría Climatización que estaba siendo mapeada en IncidenciaSeeder.
    Ahora las 7 categorías están completas.

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -14,6 +14,7 @@ class CategoriaSeeder extends Seeder
             ['nombre_categoria' => 'Fontanería', 'descripcion_categoria' => 'Problemas relacionados con tuberías, grifos y sistemas de agua'],
             ['nombre_categoria' => 'Humedades', 'descripcion_categoria' => 'Problemas de humedad, filtraciones y goteras'],
             ['nombre_categoria' => 'Calefacción', 'descripcion_categoria' => 'Reparación y mantenimiento de sistemas de calefacción'],
+            ['nombre_categoria' => 'Climatización', 'descripcion_categoria' => 'Reparación y mantenimiento de sistemas de climatización'],
             ['nombre_categoria' => 'Electricidad', 'descripcion_categoria' => 'Problemas eléctricos, instalaciones y enchufes'],
             ['nombre_categoria' => 'Otro', 'descripcion_categoria' => 'Otras categorías no especificadas'],
         ];

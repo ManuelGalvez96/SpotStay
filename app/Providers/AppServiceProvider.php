fe24a0d63c38aca9ac0e42d329c896c9f604df0c
SergiNebot <440244.joan23@fje.edu>
2026-05-14 16:03:06 +0200

    modificacion facturas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (($docRoot = $_SERVER['DOCUMENT_ROOT'] ?? null) && realpath($docRoot) !== realpath($this->app->publicPath())) {
+            $this->app->usePublicPath($docRoot);
+        }
     }
 
     /**

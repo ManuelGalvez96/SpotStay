2a6c870e0d181ed7dd5163e866d0956692ccbde8
SergiNebot <440244.joan23@fje.edu>
2026-05-18 18:11:14 +0200

    cambios en responsive miembro

--- resources/views/miembro/mapa.blade.php
+++ resources/views/miembro/mapa.blade.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>SpotStay | Mapa</title>
 		<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
-		<link rel="stylesheet" href="{{ asset('css/miembro/miembro.css') }}?v=4" />
+		<link rel="stylesheet" href="{{ asset('css/miembro/miembro.css') }}?v=5" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
 	</head>
 	<body class="pagina-mapa">

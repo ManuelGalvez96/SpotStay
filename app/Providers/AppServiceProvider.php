3bd6682b69da0e2b788edd15f05cd77d30eff0af
HugoFI <100006119.joan23@fje.edu>
2026-05-24 23:15:59 +0200

    Correccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -58,12 +58,6 @@ class AppServiceProvider extends ServiceProvider
                 // Si el plan es Gratuito muestra anuncios
                 $mostrarAnuncios = $suscripcion?->plan_suscripcion === 'Gratuito' || $suscripcion?->plan_suscripcion === "Miembro Estándar";
 
-                // Coge la suscripción más reciente del usuario
-                $suscripcion = $usuario->suscripciones()->latest('id_suscripcion')->first();
-                
-                // Si el plan es Gratuito muestra anuncios
-                $mostrarAnuncios = $suscripcion?->plan_suscripcion === 'Gratuito';
-
                 $view->with([
                     'nombreUsuario' => $nombre,
                     'tieneFoto' => $tieneFoto,
